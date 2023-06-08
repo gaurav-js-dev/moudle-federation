2970a0229081ff6881f12224ccc019f715397f5d
Gaurav <23628920+gaurav-js-dev@users.noreply.github.com>
2023-06-08 18:05:48 +0530

    Added memory history

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,6 +1,6 @@
 import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
@@ -10,16 +10,16 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "mo",
 });
 
-export default () => {
+export default ({ history }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
