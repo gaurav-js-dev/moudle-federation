e181f5aa486e3b90a67cce15ea5058528102ea62
Gaurav <23628920+gaurav-js-dev@users.noreply.github.com>
2023-06-09 19:58:53 +0530

    Implemented navigation in container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,24 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
-  useEffect(() => {
-    mount(ref.current);
-  });
-
   const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
   return <div ref={ref} />;
 };
